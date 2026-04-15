9ee1ff5921efac121778cdfe613fc62ebd2f6e38
unknown <nik.pet.vl.06@gmail.com>
2026-04-15 18:36:39 +0300

    почти доделанный тест

--- Shablon.py
+++ Shablon.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import (QMessageBox, QWidget, QVBoxLayout, QHBoxLayout, QWizardPage,
-                               QGroupBox, QPushButton, QLabel, QLineEdit)
+                               QGroupBox, QPushButton, QLabel, QLineEdit, QCheckBox)
 from PySide6.QtGui import QPixmap, Qt
 import markdown
 
@@ -38,12 +38,29 @@ class Shablon(QWizardPage):
             self.setLayout(main_layout)
 
         if(self.list[number] == 1):
-            query_label = QLabel()
+            text = load_file("course\data\\" + self.pages[self.number])
+
+            text_query = text.split("\n")[0]
+            text_answer_1 = text.split("\n")[1]
+            text_answer_2 = text.split("\n")[2]
+            text_answer_3 = text.split("\n")[3]
+            text_answer_4 = text.split("\n")[4]
+            self.answer = text.split("\n")[5]
+
+            markdown_text_query = markdown.markdown(text_query)
+            markdown_text_answer_1 = markdown.markdown(text_answer_1)
+            markdown_text_answer_2 = markdown.markdown(text_answer_2)
+            markdown_text_answer_3 = markdown.markdown(text_answer_3)
+            markdown_text_answer_4 = markdown.markdown(text_answer_4)
+
+            query_label = QLabel(markdown_text_query)
             group_layout = QGroupBox()
 
-            query_label = QLabel("cubbnkn")
+            self.answer1 = QCheckBox(markdown_text_answer_1)
+            self.answer2 = QCheckBox(markdown_text_answer_2)
+            self.answer3 = QCheckBox(markdown_text_answer_3)
+            self.answer4 = QCheckBox(markdown_text_answer_4)
 
-            self.ansver = QLineEdit()
 
             main_layout.addWidget(query_label)
             main_layout.addWidget(self.ansver)
