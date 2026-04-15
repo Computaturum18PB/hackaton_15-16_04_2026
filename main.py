f6805803d7514ed2ae19b422c0d0a8747456de3f
unknown <nik.pet.vl.06@gmail.com>
2026-04-15 10:55:27 +0300

    MainWindow с иконкой

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-"""
-Основной файл запуска проекта.
-"""
+from PySide6.QtWidgets import (QApplication, QMainWindow)
+# from PySide6.QtCore import Qt, Slot
+# from PySide6.QtGui import QFont
+from MainWindow import MainWindow
+
+
+if __name__ == '__main__':
+    app = QApplication()
+    window = MainWindow()
+    window.show()
+    app.exec_()
