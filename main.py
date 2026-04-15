17c5b3ce4321e146ff7c6bbf142a55ce9195b7d7
Computaturum18PB <ismaklakov@gmail.com>
2026-04-15 14:51:29 +0300

    last

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QLabel, QVBoxLayout, QMainWindow, QWidget
 from PySide6.QtGui import QIcon, QPixmap
 
 class MainWindow(QMainWindow):
@@ -8,6 +8,16 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Обучающий курс: \"Переработка нефти\"")
         self.setWindowIcon(QIcon(QPixmap("images/icon.png")))
         self.showMaximized()
+        
+        widget = QWidget()
+        self.setCentralWidget(widget)
+        
+        self.main_label = QLabel("Добро пожаловать на обучающий курс: Переработка нефти!")
+        
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addWidget(self.main_label)
+        
+        
 
 app = QApplication()
 window = MainWindow()
