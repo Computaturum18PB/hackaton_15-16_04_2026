9956a7d878590cd238741e62af0c30a1020bfaec
unknown <nik.pet.vl.06@gmail.com>
2026-04-16 11:08:02 +0300

    иконки работают

--- Shablon.py
+++ Shablon.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout, QWizardPage,
                                QGroupBox, QPushButton, QLabel, QLineEdit, QCheckBox,
-                               QRadioButton, QButtonGroup, QMessageBox, QScrollArea)
+                               QRadioButton, QButtonGroup, QMessageBox, QScrollArea,
+                               QSizePolicy)
 from PySide6.QtGui import QPixmap, Qt, QIcon
 from PySide6.QtCore import QSize
 import markdown
@@ -57,13 +58,15 @@ class Shablon(QWizardPage):
             scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
             scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
 
+            main_layout.addWidget(scroll, 1)
+
             content_widget = QWidget()
             content_layout = QVBoxLayout(content_widget)
 
             info_label = QLabel(markdown_text)
             info_label.setWordWrap(True)
             info_label.setTextFormat(Qt.RichText)
-            info_label.setMinimumHeight(500)
+            info_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
             basement_label = QLabel()
 
@@ -73,7 +76,6 @@ class Shablon(QWizardPage):
 
             scroll.setWidget(content_widget)
 
-            main_layout.addWidget(scroll)
             self.setLayout(main_layout)
 
         if(self.list[number] == 1):
@@ -122,50 +124,60 @@ class Shablon(QWizardPage):
             main_layout.addWidget(query_label)
             main_layout.addWidget(group_box)
             self.setLayout(main_layout)
-    
-        if (self.list[self.number] == 2):
+
+        if self.list[self.number] == 2:
+
+            scroll = QScrollArea()
+            scroll.setWidgetResizable(True)
+            scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+            scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+
+            content_widget = QWidget()
+            content_layout = QVBoxLayout(content_widget)
+            content_layout.setContentsMargins(0, 0, 0, 0)
+
+            image_path = os.path.join(self.course_path, "images", self.pictures[self.number])
+            image = QPixmap(image_path)
+
+            image_label = QLabel()
+            image_label.setAlignment(Qt.AlignCenter)
+            image_label.setScaledContents(True)
+
+            self.original_pixmap = image
+            image_label.setPixmap(image)
+
+            image_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
             legend_label = QLabel()
             legend_label.setWordWrap(True)
             legend_label.setTextFormat(Qt.RichText)
+            legend_label.setAlignment(Qt.AlignCenter)
 
-            if(self.pages[self.number] != ""):
+            if self.pages[self.number] != "":
                 file_path = os.path.join(self.course_path, "data", self.pages[self.number])
                 text = load_file(file_path)
                 markdown_text = markdown.markdown(text)
                 legend_label.setText(markdown_text)
 
-            scroll = QScrollArea()
-            scroll.setWidgetResizable(True)
-            scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-
-            content_widget = QWidget()
-            content_layout = QVBoxLayout(content_widget)
-
-            target_height = 400
-
-            image_path = os.path.join(self.course_path, "images", self.pictures[self.number])
-            image = QPixmap(image_path)
-            scaled_image = image.scaledToHeight(target_height, Qt.SmoothTransformation)
-            image_label = QLabel(self)
-            image_label.setPixmap(scaled_image)
-
-            image_size = scaled_image.size()
-            page_width = image_size.width() + 100
-            page_height = image_size.height() + 150
-            self.setFixedSize(page_width, page_height)
-
-            legend_label = QLabel()
-            legend_label.setWordWrap(True)
-            legend_label.setTextFormat(Qt.RichText)
-
-            content_layout.addWidget(image_label)
-            content_layout.addWidget(legend_label)
+            content_layout.addWidget(image_label, 1)
+            content_layout.addWidget(legend_label, 0)
 
             scroll.setWidget(content_widget)
-
             main_layout.addWidget(scroll)
             self.setLayout(main_layout)
+
+        def resizeEvent(self, event):
+            super().resizeEvent(event)
+            if hasattr(self, 'original_pixmap'):
+                available_width = self.width() - 50
+                available_height = self.height() - 100
+
+                scaled = self.original_pixmap.scaled(
+                    available_width, available_height,
+                    Qt.KeepAspectRatio,
+                    Qt.SmoothTransformation
+                )
+                self.image_label.setPixmap(scaled)
 
     def check_answer(self):
         selected_id = self.answer_group.checkedId()
